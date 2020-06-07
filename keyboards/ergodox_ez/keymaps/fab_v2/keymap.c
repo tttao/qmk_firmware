690c7fc0bd3b66f5df41dc55916bbe60c11b192c
fabrice <fabrice.monnier@protonmail.com>
2020-06-07 21:51:08 +0200

    Add dynamic macro

--- keyboards/ergodox_ez/keymaps/fab_v2/keymap.c
+++ keyboards/ergodox_ez/keymaps/fab_v2/keymap.c
@@ -53,47 +53,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_E] = ACTION_TAP_DANCE_DOUBLE(FR_EACU, FR_EGRV),
   [TD_A] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_a_accent_finished, dance_a_accent_reset)
 };
-/*
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[BASE] = LAYOUT_ergodox_pretty(
-  // left hand
-  KC_ESC,          			FR_AMPR,     FR_EACU,       FR_DQUO, FR_QUOT, FR_LPRN, KC_INS,              KC_DEL,       FR_MINS, FR_EGRV, FR_UNDS, FR_CCED, FR_AGRV,  FR_CIRC,
-  KC_TAB,          			FR_A,        FR_Z,          KC_E,    KC_R,    KC_T,    CS_COPY,              CS_CUT,       KC_Y,    TD(TD_U),    KC_I,    KC_O,    KC_P,     FR_DLR,
-  MO(MEDIA),      			FR_Q,        KC_S,          KC_D,    KC_F,    KC_G,                                       KC_H,    KC_J,    KC_K,    KC_L,    FR_M,     MO(MEDIA),
-  MT(MOD_LSFT, FR_LPRN),    FR_W, 		  KC_X,         KC_C,    KC_V,    KC_B,    CS_PASTE,             KC_TRNS,      KC_N,    FR_COMM, FR_SCLN, FR_COLN, KC_UP,    MT(MOD_RSFT, FR_RPRN),
-  KC_LCTL, 		  			KC_LGUI, 	  KC_LALT, 	    FR_EXLM, MO(SYMB),                               KC_RALT, KC_RCTRL,KC_LEFT, KC_DOWN, KC_RIGHT,
-																			 FR_EQL,  FR_SLSH,   CS_ALT_TAB,      KC_TRNS,
-																					  FR_RABK,   KC_HOME,
-																	 KC_SPC, KC_BSPC, FR_LABK,   KC_END,       KC_TRNS, KC_ENT
-),
-
-[SYMB] = LAYOUT_ergodox_pretty(
-  // left hand
-  KC_ESC,  	KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_MUTE,     KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  KC_TRNS, TD(TD_A),FR_LBRC, TD(TD_E), FR_LABK, FR_RABK, KC_VOLU,     KC_TRNS, KC_UP,   KC_KP_7, KC_KP_8, KC_KP_9,    FR_ASTR, KC_F12,
-  KC_TRNS, FR_LCBR, FR_RCBR, FR_RBRC, FR_PIPE, FR_GRV,                        KC_DOWN, KC_KP_4, KC_KP_5, KC_KP_6,    KC_PLUS, KC_TRNS,
-  KC_TRNS, FR_DLR,  FR_PERC, FR_ASTR, FR_HASH, FR_TILD, KC_TRNS,     KC_TRNS, KC_AMPR, KC_KP_1, KC_KP_2, KC_KP_3,    KC_BSLS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_KP_DOT,KC_KP_0,FR_COMM, KC_EQL,  KC_TRNS,
-                                               KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
-                                                        KC_TRNS,     KC_TRNS,
-                                      KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
-),
-
-[MEDIA] = LAYOUT_ergodox_pretty(
-  // left hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
-  KC_TRNS, KC_TRNS, KC_TRNS, FR_EGRV, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,                                         KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
-
-                                               KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
-                                                        KC_TRNS,     KC_TRNS,
-                                      KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_WBAK
-),
-};
-*/
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
     switch (keycode) {
